6d56bd8d8e0f01797e1a7a6c2c0e1a2d73bb76e4
adixime <adixime@gmail.com>
2012-02-03 11:08:59 -0500

    you can now change the images...

--- js/slideshow.js
+++ js/slideshow.js
@@ -1,7 +1,18 @@
 $(document).ready(function () {
 	
 	$('#next').on('click', function () {
-		var current = $('.slides .current')
+		var current = $('.slides .current').index();
+		var next = current + 1;
+		
+		if (next >= $('.slides img').length){
+			next = 0;
+		}
+		
+		$('.slides .current').removeClass('current');
+		$('.slides img').eq(next).addClass('current');
+		
+		console.log(next);
+	
 	});
 	
 	
